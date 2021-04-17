dd6a8804a103bc316432723b5ef30a13b135a999
Prashant Bhat <prashantbhat84@gmail.com>
2021-04-17 14:28:00 +0530

    added use state for chaanging title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
+import { useState } from 'react'
 const ExpenseItem = ({ date, title, amount }) => {
-    const expenseTitle = title;
+    const [title1, setTitle1] = useState(title)
+
     const expenseAmount = amount;
+    const buttonClicked = (event) => {
+        setTitle1("Updated");
+
+
+    }
     return <Card className="expense-item">
         <ExpenseDate date={date} />
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
+            <h2>{title1}</h2>
             <div className="expense-item__price">Rs {expenseAmount} /-</div>
         </div>
+        <button onClick={buttonClicked}>Change title</button>
 
     </Card>
 
