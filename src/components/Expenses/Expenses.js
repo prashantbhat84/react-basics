03c571735253c0091d84aadfac0d8fe7aa312e89
Prashant Bhat <prashantbhat84@gmail.com>
2021-05-08 16:13:59 +0530

    Assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,26 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import { useState } from 'react'
+
+import ExpensesFilter from './ExpenseFilter';
+
+
 const Expenses = ({ expenses }) => {
+    const [selected, setSelected] = useState('')
+
+    const yearPicker = (e) => {
+
+        setSelected(e.target.value)
+    }
+    // const filteredexpense = (expenses => {
+    //     return expenses.date.getFullYear() === selected
+    // })
+    console.log(selected);
     return <Card className="expenses">
-        <ExpenseItem title={expenses[0].title}
-            amount={expenses[0].amount}
-            date={expenses[0].date} />
-        <ExpenseItem title={expenses[1].title}
-            amount={expenses[1].amount}
-            date={expenses[1].date} />
-        <ExpenseItem title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date} />
-        <ExpenseItem title={expenses[3].title}
-            amount={expenses[3].amount}
-            date={expenses[3].date} />
+        <ExpensesFilter selected={selected} yearPicker={yearPicker} />
+
+        {expenses.map((item) => <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />)}
     </Card>
 
 }
